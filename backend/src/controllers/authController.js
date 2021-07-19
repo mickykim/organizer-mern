0660eacd0ac3c7327f4111847c95749dc9b487c1
Miqueas Kim <miqueaskimhan@gmail.com>
2021-07-19 13:12:51 -0500

    Added keypair generating script

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -34,3 +34,18 @@ export const loginUser = (req, res, next) => {
         });
     });
 };
+
+export const authenticateToken = (req, res, next) => {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    if (token == null) {
+        return;
+    }
+    jwt.verify(token, keys.JWT_SECRET, (err, user) => {
+        if (err) {
+            return res.sendStatus(403);
+        }
+        req.user = user;
+        next();
+    });
+};
